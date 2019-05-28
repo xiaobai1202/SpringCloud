2a9f806d12752ff8a124bbe9c0481b47586fb332
811105717 <811105717@qq.com>
2019-05-28 13:45:34 +0800

    2019/05/28 11:35 更新README.MD

---
+++ cloud-eureka-7003/src/main/java/com/xiaobai/cloudeureka7003/CloudEureka7003Application.java
@@ -0,0 +1,19 @@
+package com.xiaobai.cloudeureka7003;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 自动生成
+ * @author IDEA
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class CloudEureka7003Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudEureka7003Application.class, args);
+    }
+
+}
